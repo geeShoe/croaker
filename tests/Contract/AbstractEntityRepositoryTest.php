a90aeccc9e2f6a8784e505b0a6d06e6bc5e0cb3b
Jesse Rushlow <jr@geeshoe.com>
2020-01-02 17:24:15 -0500

    Created project status repo

---
+++ tests/Contract/AbstractEntityRepositoryTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright 2020 Jesse Rushlow - geeShoe Development
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace App\Tests\Contract;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AbstractEntityRepositoryTest
+ *
+ * @package App\Tests\Contract
+ * @author  Jesse Rushlow <jr@geeshoe.com>
+ */
+abstract class AbstractEntityRepositoryTest extends TestCase implements EntityRepositoryUnitTestInterface
+{
+    public string $repositorySignature;
+
+    /**
+     * Set the signature of the Repository under test
+     *
+     * @param string $signature
+     */
+    public function setRepositorySignature(string $signature): void
+    {
+        $this->repositorySignature = $signature;
+    }
+
+    /**
+     * @dataProvider repositoryMethodDataProvider
+     * @param string $methodName
+     */
+    public function testRepositoryHasMethodDefined(string $methodName): void
+    {
+        self::assertIsCallable([$this->repositorySignature, $methodName]);
+    }
+}
